64ff607d18ccf5a5f058db6564db95ca4d05a83b
hovy1994 <37677421+hovy1994@users.noreply.github.com>
2024-08-08 16:58:07 +0900

    불필요한 코드 제거

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -2,13 +2,14 @@ class Solution(object):
     def restoreMatrix(self, rowSum, colSum):
         rowLen = len(rowSum)
         colLen = len(colSum)
-        curRowSum = [0 for _ in range(rowLen)]
-        curColSum = [0 for _ in range(colLen)]
-        result = [[[] for _ in range(colLen)] for _ in range(rowLen)]
+        
+        result = [[0 for _ in range(colLen)] for _ in range(rowLen)]
+        
         for i in range(rowLen):
             for j in range(colLen):
-                result[i][j] = min(rowSum[i] - curRowSum[i], colSum[j] - curColSum[j])
-                curRowSum[i] += result[i][j]
-                curColSum[j] += result[i][j]
-
+                min_value = min(rowSum[i], colSum[j])
+                result[i][j] = min_value
+                rowSum[i] -= min_value
+                colSum[j] -= min_value
+        
         return result
