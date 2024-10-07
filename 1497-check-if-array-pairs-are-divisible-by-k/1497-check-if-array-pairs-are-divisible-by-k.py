80723a3ff650eb4f1e3d441f9d4542954db9ca8e
hovy1994 <37677421+hovy1994@users.noreply.github.com>
2024-10-07 17:19:57 +0900

    Time: 553 ms (21.96%), Space: 26.1 MB (19.63%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -5,22 +5,19 @@ class Solution(object):
             mod = num%k
             dict[mod] = dict.get(mod, 0)+1
             
-        for key in dict:
-            if dict[key]==0:
-                continue
-            complement = (k-key)%k
+        while dict:
+            mod, count = dict.popitem()
+            complement = (k-mod)%k
             
-            if key==complement:
-                if dict[key]%2==0:
-                    dict[complement]=0
+            if mod==complement:
+                if count%2==0:
                     continue
                 else:
                     return False
-            if dict.get(key, 0)==dict.get(complement,0):
-                dict[key]=0
-                dict[complement]=0
+            if count==dict.get(complement,0):
+                dict.pop(complement)
                 continue
             else:
                 return False
-        
+            
         return True
